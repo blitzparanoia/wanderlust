e289bab18338ff1c5900586fc85a3c59578e16b7
blitzparanoia <bluemilk44@gmail.com>
2019-06-11 19:29:53 -0400

    mod filenames, app con has helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,9 @@ class ApplicationController < Sinatra::Base
       !!current_user
     end
 
-    # def current_user
-    #   @current_user = User.find
+    def current_user
+      @current_user = User.find(session[:user_id])
+    end
 
 
 end
