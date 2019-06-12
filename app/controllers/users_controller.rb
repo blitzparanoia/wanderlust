5b3923269cd25dcbf848471f50a75093c764cb2c
blitzparanoia <bluemilk44@gmail.com>
2019-06-12 19:07:13 -0400

    building sign up get and post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    if logged_in?
-      redirect to '/destination'
-    else
+     # if logged_in?
+     #   redirect to '/destination'
+     # else
       erb :"user/new"
+   # end
+end
+
+  post '/signup' do
+    if params[:username].empty? || params[:password].empty?
+      @user = User.create(params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect '/destination'
+      else
+        redirect '/signup'
   end
+end
 end
 
 end
