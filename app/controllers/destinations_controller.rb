9dc0ef2a61cec5fc89106f9dccbdbb0e14d8487f
blitzparanoia <bluemilk44@gmail.com>
2019-06-20 00:35:38 -0400

    mod erb view files, removed slug from user controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,81 +1,81 @@
 class DestinationsController < ApplicationController
 
-    get "/destination" do
-  # if logged_in?
-  #   @user = User.find(session[:user_id])
-  #   @destination = Destination.all
-  # else
-  #   redirect '/login'
-  # end
-  # erb :"/destination/show"
-  @destination = Destination.all
-  erb :'/destination/show'
-  end
-
-  get "/destination/new" do
-  if logged_in?
-    @user = User.find(session[:user_id])
-  else
-    redirect '/login'
-  end
-
-  erb :"/destination/new"
-end
-
-    post '/destination' do
- if !logged_in?
-      redirect to '/login'
-    end
-
-    if params[:city] != ""
-      @destination = Destination.create(city: params[:city], user_id: current_user.id)
-
-      redirect to "/destination/#{@destination.id}"
-    else
-      redirect to '/destination/new'
-    end
-end
-
-        get '/destination/:id' do
-          @destination = Destination.find_by(id: params[:id])
-          erb :'/destination/show'
-        end
-
-    get '/destination/:id/edit' do
-        @destination = Destination.find_by(id: params[:id])
-   if logged_in?
-     if @destination.user == current_user
-       erb :'/destination/edit'
-     else
-       redirect "/user/#{@destination.id}"
-     end
-   else
-     redirect '/login'
-   end
-  end
-
-
-  patch '/destination/:id' do
-      @destination = Destination.find(id: params[:id])
-      if logged_in?
-  if @destination.user == current_user && params[:city] != ""
-
-     @destination.update(city: params[:city])
-     redirect to "/destination/#{@destination.id}"
-  else
-     redirect to "/user/#{@destination.id}"
-  end
-else
-  redirect to '/login'
-end
-end
-
-  delete '/destination/:id' do
-    @destination = Destination.find(id: params[:id])
-    if @destination.user == current_user
-      @destination.destroy
-      redirect to '/destination'
-    else
-      redirect to '/destination'
-    end
+#     get "/destination" do
+#   # if logged_in?
+#   #   @user = User.find(session[:user_id])
+#   #   @destination = Destination.all
+#   # else
+#   #   redirect '/login'
+#   # end
+#   # erb :"/destination/show"
+#   @destination = Destination.all
+#   erb :'/destination/show'
+#   end
+#
+#   get "/destination/new" do
+#   if logged_in?
+#     @user = User.find(session[:user_id])
+#   else
+#     redirect '/login'
+#   end
+#
+#   erb :"/destination/new"
+# end
+#
+#     post '/destination' do
+#  if !logged_in?
+#       redirect to '/login'
+#     end
+#
+#     if params[:city] != ""
+#       @destination = Destination.create(city: params[:city], user_id: current_user.id)
+#
+#       redirect to "/destination/#{@destination.id}"
+#     else
+#       redirect to '/destination/new'
+#     end
+# end
+#
+#         get '/destination/:id' do
+#           @destination = Destination.find_by(id: params[:id])
+#           erb :'/destination/show'
+#         end
+#
+#     get '/destination/:id/edit' do
+#         @destination = Destination.find_by(id: params[:id])
+#    if logged_in?
+#      if @destination.user == current_user
+#        erb :'/destination/edit'
+#      else
+#        redirect "/user/#{@destination.id}"
+#      end
+#    else
+#      redirect '/login'
+#    end
+#   end
+#
+#
+#   patch '/destination/:id' do
+#       @destination = Destination.find(id: params[:id])
+#       if logged_in?
+#   if @destination.user == current_user && params[:city] != ""
+#
+#      @destination.update(city: params[:city])
+#      redirect to "/destination/#{@destination.id}"
+#   else
+#      redirect to "/user/#{@destination.id}"
+#   end
+# else
+#   redirect to '/login'
+# end
+# end
+#
+#   delete '/destination/:id' do
+#     @destination = Destination.find(id: params[:id])
+#     if @destination.user == current_user
+#       @destination.destroy
+#       redirect to '/destination'
+#     else
+#       redirect to '/destination'
+#     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,35 +18,28 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/login' do
-    if !logged_in?
-     erb :'user/login'
-   else
-     redirect '/destination'
-   end
-  end
+  # get '/login' do
+  #   if !logged_in?
+  #    erb :'user/login'
+  #  else
+  #    redirect '/destination'
+  #  end
+  # end
 
   post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect '/destination'
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/user/#{@user.id}"
     else
       redirect '/login' #try again
     end
   end
 
   get '/logout' do
-    if logged_in?
       #the user logs out
       session.clear
       redirect '/'
     end
-  end
-
-  get '/user/:slug' do
-  @user = User.find_by_slug(params[:slug])
-   erb :'user/show'
-  end
 
 end
