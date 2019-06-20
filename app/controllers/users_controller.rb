df4216d6859e8c40c554973bf17d2e927c3f6c59
blitzparanoia <bluemilk44@gmail.com>
2019-06-20 18:36:20 -0400

    Fixed typo, closed form tag on edit erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     else
       @user = User.create(:username => params[:username], :password => params[:password])
       session[:user_id] = @user.id
-      redirect '/destination'
+      redirect '/user/show'
     end
   end
 
